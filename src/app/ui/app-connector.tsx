34e97f429c7cff9eb53433151743716eebb5d8ca
Vladislav Panov <vp@Vladislavs-MacBook-Air.local>
2022-08-07 22:34:22 +0200

    feat: added react query

--- src/app/ui/app-connector.tsx
+++ src/app/ui/app-connector.tsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { ThemeProvider } from '@shared/ui';
 
 import { App } from './app';
 
+const queryClient = new QueryClient();
+
 export const AppConnector = () => {
   return (
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
