5f95ef6a5a65c13905f2125ec18b2e6913e00043
Vladislav Panov <pn@kode.ru>
2022-08-15 12:30:14 +0200

    chore: release for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@features': resolve(__dirname, 'src/features'),
